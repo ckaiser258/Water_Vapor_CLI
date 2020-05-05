08bc3b2ec51b8338a7bddc2fb028161ba857cb71
ckaiser258 <kaiserc2@seattleu.edu>
2020-05-05 12:09:40 -0700

    added api and changed environment

--- app/adapter/gamesapi.rb
+++ app/adapter/gamesapi.rb
@@ -1,3 +1,34 @@
+require 'net/https'
+
 class GamesApi
-    
+    def self.get_platform
+        http = Net::HTTP.new('api-v3.igdb.com',443)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => ''})
+        request.body = 'fields name;'
+        puts http.request(request).body
+        # books = json_response["items"].map do |item| 
+        #   {description: item["volumeInfo"]["description"],
+        #   title: item["volumeInfo"]["title"]}
+        # end
+        # binding.pry
+      end
+
+    def self.get_genre
+        http = Net::HTTP.new('api-v3.igdb.com',443)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/genres'), {'user-key' => ''})
+        request.body = 'fields name;'
+        puts http.request(request).body
+    end
+
+    def self.get_games
+        http = Net::HTTP.new('api-v3.igdb.com',443)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/games'), {'user-key' => ''})
+        request.body = 'fields name; where name = "Halo: Combat Evolved";'
+        puts http.request(request).body
+    end
 end
+
+GamesApi.get_games
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'net/https'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
