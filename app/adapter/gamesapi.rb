7216681b48122157f955671c7046c0dbca4ded7e
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-06 16:58:15 -0700

    Moved all require to enviorment, add Game methods

--- app/adapter/gamesapi.rb
+++ app/adapter/gamesapi.rb
@@ -1,12 +1,11 @@
-require 'net/https'
-require 'pry'
-require 'json'
-
 class GamesApi
+    
+    @@user_key = "604d3e58cf35c387d2cc94b3e51ecc35"
+
     def self.get_games(count)
         http = Net::HTTP.new('api-v3.igdb.com',443)
         http.use_ssl = true
-        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/games'), {'user-key' => '604d3e58cf35c387d2cc94b3e51ecc35'})
+        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/games'), {'user-key' => @@user_key})
         request.body = "fields name, first_release_date, rating, summary; limit #{count};"
         response_string = http.request(request).body
         response_array = JSON.parse(response_string)
@@ -15,11 +14,9 @@ class GamesApi
     def self.get_consoles(count)
         http = Net::HTTP.new('api-v3.igdb.com',443)
         http.use_ssl = true
-        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => '604d3e58cf35c387d2cc94b3e51ecc35'})
+        request = Net::HTTP::Post.new(URI('https://api-v3.igdb.com/platforms'), {'user-key' => @@user_key})
         request.body = "fields name; limit #{count};"
         response_string = http.request(request).body
         response_array = JSON.parse(response_string)
     end
 end
-
-GamesApi.get_consoles(20)
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 require 'net/https'
+require 'json'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
