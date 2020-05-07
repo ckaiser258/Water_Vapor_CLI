2ac735ed41c8fc5187ef08cc78bbfb65d875bebb
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-06 17:52:04 -0700

    Turned off activerecord logger in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,8 @@ require 'json'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+
+# Turn off active record SQL printing to the terminal
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app/adapter'
